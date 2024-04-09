7deb05f59073edeea31211f74c145beaa1d2b240
AS <arssoo569@gmail.com>
2024-04-09 15:59:26 +0200

    clearing of Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,15 @@
 import styles from './Button.module.scss';
 import clsx from 'clsx';
+import propTypes from 'prop-types';
 
 const Button = (props) => {
     return (<button type="button" onClick={props.action} className={clsx(styles.button, props.className)}>{props.children}</button>);
+};
+
+Button.propTypes = {
+    action: propTypes.func.isRequired,
+    children: propTypes.node.isRequired,
+    className: propTypes.string
 };
 
 export default Button;
